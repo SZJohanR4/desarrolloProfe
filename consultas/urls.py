31551329c389eade14812ded173d953ee25c72f2
SZJohanR4 <johan_szr@hotmail.com>
2016-10-15 17:50:13 -0500

    primer insertar

--- consultas/urls.py
+++ consultas/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'index/$', views.logeo, name='index'),
-    url(r'home/$', views.homeAdmin, name='homeAdmin'),
+    url(r'home/$', views.homeDP, name='homeDP'),
     url(r'logout/$', views.logout, name='logout'),
     url(r'crearProyecto/$', views.crearProyecto, name='crearProyecto'),
-     url(r'crearActividad/$', views.crearActividad, name='crearActividad'),
+    url(r'crearActividad/$', views.crearActividad, name='crearActividad'),
+    
 ]
--- consultas/views.py
+++ consultas/views.py
@@ -7,8 +7,8 @@ from django.forms.forms import Form
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import Usuario
-
+from .models import Usuario, Actividad, Estudiante, Proyecto
+from consultas.models import Usuario
 # Create your views here.
 
 
@@ -19,6 +19,7 @@ def logeo(request):
     saludo=None
     if request.method=="POST":#mi request bien con datos
         form=loginForm(request.POST)#le paso los datos del formulario
+        
         if form.is_valid():#pregunto si los datos del form son validos
             try:
                 user=Usuario.objects.get(usuario=request.POST['user'])
@@ -32,6 +33,7 @@ def logeo(request):
                             if user.rol=="Director de Proyecto":
                                 request.session["usuario"]=user.usuario
                                 request.session["nombre"]=user.nombre
+                            
                                 return render(request,"consultas/PaginaPrincipalDirProyecto.html")
                             else:
                                 if user.rol=="Estudiante":
@@ -52,28 +54,46 @@ def logeo(request):
                 return render(request,"consultas/index.html",context)
     else:
         form=loginForm()
-        message="Datos invalidos"
-        context={'message':message}
-    return render(request,'consultas/index.html', context)
+    return render(request,'consultas/index.html')
 
 
 
 
 
 
-def homeAdmin(request):
-    datosUser=None
-    #tenemos que traer el id del user
-    context={'datosUser':datosUser}
-    return render_to_response('consultas/PaginaPrincipalAdmin.html', )
+def homeDP(request):
+    return render(request,"consultas/PaginaPrincipalDirProyecto.html")
 
 def crearProyecto(request):
     return render(request,'consultas/CrearProyecto.html')
 
+
+
 def crearActividad(request):
-    message=request.session['usuario']
-    context={'message':message}
-    return render(request,'consultas/crearActividad.html')
+    #message=request.session['usuario']
+    #context={'message':message}
+    
+    if request.method=="POST":
+        actividad=Actividad()
+        try:
+            actividad.nombre=request.POST['nombreActividad']
+            actividad.descripcion=request.POST['descripcion']
+            actividad.fecha_Limite=request.POST['fechaLimite']
+            actividad.nota=request.POST['nota']
+          #  actividad.adjunto=
+            user=Usuario.objects.get(usuario=request.POST['idDirector'])
+            estudiante=Estudiante.objects.get(id=request.POST['idEstudiante'])
+            proyecto=Proyecto.objects.get(id=request.POST['idProyecto'])
+            actividad.idDirector=user
+            actividad.idEstudiante=estudiante
+            actividad.idProyecto=proyecto
+            actividad.save()
+        except KeyError:
+            datosUser=KeyError
+            context={'datosUser':datosUser}
+            return render(request,"consultas/PaginaPrincipalDirProyecto.html")
+    else:
+        return render(request,'consultas/crearActividad.html')
 
 def logout(request):
     try:
@@ -82,6 +102,6 @@ def logout(request):
     except KeyError:
         pass
         saludo="Gracias por su visita"
-        context={'saludo':saludo}
-    return render(request,'index.html', context)
+        
+    return render(request,'consultas/index.html')
     
